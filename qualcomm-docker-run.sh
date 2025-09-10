7ac8c51c9f704792f89f7b10cf22a0391562fc40
Jaime <jaime.spencer@imd-tec.com>
2025-09-10 14:01:40 +0100

    Added container mount folder and bitbake script

--- qualcomm-docker-run.sh
+++ qualcomm-docker-run.sh
@@ -1,46 +1,65 @@
 #!/bin/bash
 
-#ensure that script exits on error and unset variables are errors
-set -euo pipefail
-
+# Function to display usage
 usage() {
-    echo "Usage: $0 --container-name <name> --bsp-dir <path> --docker-ver <version>"
+    echo "Usage: $0 --container-name <name> [--usessh] [--sdk] [--swu] --manifest-repo <repo_url> --manifest-branch <branch_name> --manifest-xml <filename>"
     exit 1
 }
 
-#parse command line arguments
-while [[ $# -gt 0 ]]; do
+while [[ "$#" -gt 0 ]]; do
     case $1 in
+        --usessh) usessh=1 ;;
+        --sdk) sdk=1 ;;
+        --swu) swu=1 ;;
+        --v2n) v2n=1 ;;
         --container-name) container_name="$2"; shift ;;
-
-        *) echo "Unknown option $1"; usage ;;
+        --manifest-repo) manifest_repo="$2"; shift ;;
+        --manifest-xml) manifest_xml="$2"; shift ;;
+        --manifest-branch) manifest_branch="$2"; shift ;;
+        *) echo "Unknown parameter passed: $1"; usage ;;
     esac
     shift
 done
 
-#if variables not set, show usage and exit
-if [[ -z "${container_name}" ]]; then
+# Validate arguments
+if [[ -z "$container_name" || -z "$manifest_repo" || -z "$manifest_xml" || -z "$manifest_branch" ]]; then
+    echo "Error: Missing required arguments."
     usage
 fi
 
-
 # Display the provided arguments
-# echo "Using SSH: $usessh"
-# echo "Build SDK: $sdk"
-# echo "Build SWU: $swu"
-# echo "Build for V2N: $v2n"
-# echo "Manifest Repo: $manifest_repo"
-# echo "Manifest Branch: $manifest_branch"
-# echo "Manifest XML: $manifest_xml"
+echo "Using SSH: $usessh"
+echo "Build SDK: $sdk"
+echo "Build SWU: $swu"
+echo "Build for V2N: $v2n"
 echo "Container Name: $container_name"
+echo "Manifest Repo: $manifest_repo"
+echo "Manifest Branch: $manifest_branch"
+echo "Manifest XML: $manifest_xml"
 
+# # Set SSH_DIR if usessh is enabled
+# if [[ "$usessh" -eq 1 ]]; then
+#     echo "# Horrid .ssh permissions hack"
+#     echo "# setting SSH_DIR to ~/ssh_1000 if it exists, else ~/.ssh"
+#     if [ -d ~/ssh_1000 ]; then
+#         SSH_DIR=~/ssh_1000
+#     else
+#         SSH_DIR=~/.ssh
+#     fi
+# fi
+
+# set -e
 
 # echo "# Running Qualcomm container: $container_name"
 docker run -d --rm \
     --name "$container_name" \
         -v $(pwd)/for_docker:/workflows \
     imdtec/imdt-qualcomm-build-setup:0.5.1 \
+    # ${usessh:+-v ${SSH_DIR}:/home/imdt/.ssh:ro} \
     sleep infinity
 
-# docker run -it -d --rm -v ${BSP_DIR}/:/Qualcomm --name qualcomm-build-env imdtec/imdt-qualcomm-build-setup:${DOCKER_VERSION}
+bitbake_command="/workflows/bitbake-build.sh --manifest-repo ${manifest_repo} --manifest-branch ${manifest_branch} --manifest-xml ${manifest_xml} ${sdk:+--sdk} ${swu:+--swu} ${v2n:+--v2n}"
+
+
+
 
