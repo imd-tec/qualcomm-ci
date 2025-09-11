c707853ece2e52a332c036dbcb4fb03cb466796f
Jaime <jaime.spencer@imd-tec.com>
2025-09-11 16:19:48 +0100

    Added inline passing of host IDs to sudo

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #Commands
 echo "====================BITBAKE-BUILD COMMANDS========================="
-echo "CONTAINER USER DETAILS"
-echo "PWD: $(pwd)"
-echo "IDs: $(id)"
+echo "DETAILS"
+echo "CONTAINER PWD: $(pwd)"
+echo "CONTAINER USER IDs: $(id)"
+echo "HOST IDs: $HOST_UID $HOST_GID"
 echo "==================================================================="
 
 set -euo pipefail -x
@@ -12,13 +13,15 @@ ls /home/dev/
 ls /home/dev/tools/
 export QCOM_ROOT_DIR=/Qualcomm/qcs8550-le-1-0_amss_standard_oem_apqgps
 /home/dev/tools/build_netrc.sh
+
 # cat /home/dev/.netrc
 
 #Initialize user if UID/GID == 1000
 if [ "$HOST_UID" != $(id -u) ] || [ "$HOST_GID" != $(id -g) ]; then
     echo "Host and container user IDs do not match. Executing user initialization scripts."
     id
-    sudo bash /home/dev/tools/new_user_setup_1.sh
+    # sudo bash /home/dev/tools/new_user_setup_1.sh
+    sudo env HOST_UID="$HOST_UID" HOST_GID="$HOST_GID" bash -lc 'bash /home/dev/tools/new_user_setup_1.sh'
     id
 fi
 # source /home/host/.bashrc
