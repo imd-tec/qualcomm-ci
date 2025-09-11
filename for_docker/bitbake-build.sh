a5cc1035bd968dffe69a366291dd40c4eed47b77
Jaime <jaime.spencer@imd-tec.com>
2025-09-11 14:32:38 +0100

    debugging

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -45,10 +45,5 @@ $QCOM_ROOT_DIR
 # cat /home/dev/.netrc
 
 #Synchronise Repos
-if [ -z "$(ls -A /mnt/nvme1/qcom_ci/builds/*patch* 2>/dev/null)" ]; then 
-    echo "Error: no patches found in /mnt/nvme1/qcom_ci/builds/"
-    exit 1
-fi
-
 tar -xf /Qualcomm/patches.tar.gz -C /Qualcomm/
 ls /Qualcomm/
