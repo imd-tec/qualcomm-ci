a8185f1cc4567cedfff4a5d012c7304195a79f22
Jaime <jaime.spencer@imd-tec.com>
2025-09-11 16:06:49 +0100

    Added sudo to new user script execution

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -18,7 +18,7 @@ export QCOM_ROOT_DIR=/Qualcomm/qcs8550-le-1-0_amss_standard_oem_apqgps
 if [ "$HOST_UID" != $(id -u) ] || [ "$HOST_GID" != $(id -g) ]; then
     echo "Host and container user IDs do not match. Executing user initialization scripts."
     id
-    bash /home/dev/tools/new_user_setup_1.sh
+    sudo bash /home/dev/tools/new_user_setup_1.sh
     id
 fi
 # source /home/host/.bashrc
@@ -26,4 +26,4 @@ fi
 
 #Patch and Synchronise Repos
 # tar -xf /Qualcomm/patches.tar.gz -C /Qualcomm/
-# ls /Qualcomm/
+# ls /Qualcomm/ 
