8fc7d65b91cd3f3ed3324994e01e968ddd9fbfea
Jaime <jaime.spencer@imd-tec.com>
2025-09-11 16:21:35 +0100

    debugging

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -23,6 +23,8 @@ if [ "$HOST_UID" != $(id -u) ] || [ "$HOST_GID" != $(id -g) ]; then
     # sudo bash /home/dev/tools/new_user_setup_1.sh
     sudo env HOST_UID="$HOST_UID" HOST_GID="$HOST_GID" bash -lc 'bash /home/dev/tools/new_user_setup_1.sh'
     id
+    sudo ls /etc/sudoers
+    sudo getent group
 fi
 # source /home/host/.bashrc
 
