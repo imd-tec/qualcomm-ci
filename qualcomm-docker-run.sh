dc3a72ef978977d895ccb1b0c5b0b5975614c512
Jaime <jaime.spencer@imd-tec.com>
2025-09-11 12:15:39 +0100

    Removed container arguments

--- qualcomm-docker-run.sh
+++ qualcomm-docker-run.sh
@@ -66,7 +66,7 @@ docker run -d --rm \
     imdtec/imdt-qualcomm-build-setup:0.5.1 \
     sleep infinity
 
-docker exec -it "$container_name" bash -lc '
+docker exec "$container_name" bash -c '
     ls /Qualcomm
     ls /home/dev/
     export QCOM_ROOT_DIR=/Qualcomm/qcs8550-le-1-0_amss_standard_oem_apqgps
