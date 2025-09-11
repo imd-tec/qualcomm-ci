f00c07427173f4863eced78fe1aa1c056232ae32
Jaime <jaime.spencer@imd-tec.com>
2025-09-11 14:59:19 +0100

    Debugging

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -40,7 +40,7 @@ set -euo pipefail -x
 ls /Qualcomm
 ls /home/dev/
 export QCOM_ROOT_DIR=/Qualcomm/qcs8550-le-1-0_amss_standard_oem_apqgps
-$QCOM_ROOT_DIR
+echo $QCOM_ROOT_DIR
 /home/dev/build_netrc.sh
 # cat /home/dev/.netrc
 
