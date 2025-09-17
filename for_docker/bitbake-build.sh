f78284b367c96f815adc0f63e2da70a2f8b34381
Jaime <jaime.spencer@imd-tec.com>
2025-09-17 10:55:14 +0100

    Added -i flag to specify patch; changing to modern patch style

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -59,7 +59,7 @@ ls /Qualcomm/
 
 PATCHFILE="/Qualcomm/patches/sync_snap_v2_remove_chipcode_copy.patch"
 TARGET="${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/sync_snap_v2.sh"
-patch --batch -N $TARGET $PATCHFILE
+patch --batch -N -i "$TARGET" "$PATCHFILE"
 
 ~/build_scripts/sync_repos.sh -u $MANI_REPO -b $MANI_BRANCH -m $MANI_XML
 
