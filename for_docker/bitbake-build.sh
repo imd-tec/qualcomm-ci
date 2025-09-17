687dd42a902daf01805351fdea42376aa512b837
Jaime <jaime.spencer@imd-tec.com>
2025-09-17 10:01:04 +0100

    Debugging

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -20,7 +20,7 @@ export MANI_BRANCH="$MANI_BRANCH"
 export MANI_XML="$MANI_XML" 
 export QCOM_ROOT_DIR="/Qualcomm/${SRC_REPO}"
 
-ls /Qualcomm
+ls /Qualcomm/
 ls /home/dev/
 ls /home/dev/tools/
 
@@ -53,11 +53,10 @@ echo $MANI_REPO
 echo $MANI_BRANCH
 echo $MANI_XML
 
-
 #patch and synchronise Repos
 tar -xf /Qualcomm/patches.tar.gz -C /Qualcomm/
-ls /Qualcomm
-patch ${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/sync_snap_v2.sh \ /Qualcomm/patches/sync_snap_v2_remove_chipcode_copy.patch
+ls /Qualcomm/
+patch ${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/sync_snap_v2.sh /Qualcomm/patches/sync_snap_v2_remove_chipcode_copy.patch
 ~/build_scripts/sync_repos.sh -u $MANI_REPO -b $MANI_BRANCH -m $MANI_XML
 
 # #configure kernel directories
--- qualcomm-docker-run.sh
+++ qualcomm-docker-run.sh
@@ -2,7 +2,7 @@
 
 #soem arguments are from Lewis' ver that will probably not be needed...
 usage() {
-    echo "Usage: $0 --container-name <name> [--usessh] [--sdk] [--swu] --source-repo <repo_name> --manifest-repo <repo_url> --manifest-branch <branch_name> --manifest-xml <filename>"
+    echo "Usage: $0 --container-name <name> [--usessh] [--sdk] [--swu] --source-repo <repo_name> --docker-version <setup_version_num> --manifest-repo <repo_url> --manifest-branch <branch_name> --manifest-xml <filename>"
     exit 1
 }
 
@@ -13,6 +13,7 @@ while [[ "$#" -gt 0 ]]; do
         --swu) swu=1 ;;
         --v2n) v2n=1 ;;
         --container-name) container_name="$2"; shift ;;
+        --docker-version) docker_version="$2"; shift ;;
         --source-repo) source_repo="$2"; shift ;;
         --manifest-repo) manifest_repo="$2"; shift ;;
         --manifest-xml) manifest_xml="$2"; shift ;;
@@ -23,7 +24,7 @@ while [[ "$#" -gt 0 ]]; do
 done
 
 # Validate arguments
-if [[ -z "$container_name" || -z "$source_repo" || -z "$manifest_repo" || -z "$manifest_xml" || -z "$manifest_branch" ]]; then
+if [[ -z "$container_name" || -z "$source_repo" || -z "$docker_version" || -z "$manifest_repo" || -z "$manifest_xml" || -z "$manifest_branch" ]]; then
     echo "Error: Missing required arguments."
     usage
 fi
@@ -34,6 +35,7 @@ echo "Build SDK: $sdk"
 echo "Build SWU: $swu"
 echo "Build for V2N: $v2n"
 echo "Container Name: $container_name"
+echo "Build version: $docker_version"
 echo "Source repo: $source_repo"
 echo "Manifest Repo: $manifest_repo"
 echo "Manifest Branch: $manifest_branch"
@@ -72,7 +74,7 @@ docker run -d --rm \
     -v "$PWD/for_docker:/workflows"\
      -v "$CI_DIR/builds/:/Qualcomm" \
      -v "$CI_DIR/scripts/:/home/dev/tools/" \
-    imdtec/imdt-qualcomm-build-setup:0.5.1 \
+    imdtec/imdt-qualcomm-build-setup:$docker_version\
     sleep infinity
 
 # bitbake_command="bash /workflows/bitbake-build.sh --manifest-repo ${manifest_repo} --manifest-branch ${manifest_branch} --manifest-xml ${manifest_xml} ${sdk:+--sdk} ${swu:+--swu} ${v2n:+--v2n}"
