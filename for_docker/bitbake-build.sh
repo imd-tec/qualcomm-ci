094f13c8e5f1f77f52d81268db66dafb3a3e0a1e
Jaime <jaime.spencer@imd-tec.com>
2025-09-17 11:12:35 +0100

    Changed manifest xml from default to v1.0.0

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Commands
-set -euo pipefail -x
+set -euo pipefail
 
 echo " "
 echo "====================BITBAKE-BUILD COMMANDS========================="
@@ -53,14 +53,16 @@ echo $MANI_REPO
 echo $MANI_BRANCH
 echo $MANI_XML
 
-#patch and synchronise repos
+#PATCH AND SYNCHRONISE REPOS
 tar -xf /Qualcomm/patches.tar.gz -C /Qualcomm/
-ls /Qualcomm/
+# ls /Qualcomm/
 
+echo "PATCHING"
 PATCHFILE="/Qualcomm/patches/sync_snap_v2_remove_chipcode_copy.patch"
 TARGET="${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/sync_snap_v2.sh"
 patch --batch "$TARGET" "$PATCHFILE" 
 
+echo "SYNCHRONISING REPOS"
 ~/build_scripts/sync_repos.sh -u $MANI_REPO -b $MANI_BRANCH -m $MANI_XML
 
 # #configure kernel directories
