eea1e52706d2e35b9a6ba8511b76420ed4af3a9a
Jaime <jaime.spencer@imd-tec.com>
2025-09-17 10:48:46 +0100

    Added batch and forward flagfor patch to bypass exit error

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -53,11 +53,15 @@ echo $MANI_REPO
 echo $MANI_BRANCH
 echo $MANI_XML
 
-#patch and synchronise Repos
+#patch and synchronise repos
 tar -xf /Qualcomm/patches.tar.gz -C /Qualcomm/
 ls /Qualcomm/
-patch ${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/sync_snap_v2.sh /Qualcomm/patches/sync_snap_v2_remove_chipcode_copy.patch
-~/build_scripts/sync_repos.sh -u $MANI_REPO -b $MANI_BRANCH -m $MANI_XML
+
+PATCHFILE="/Qualcomm/patches/sync_snap_v2_remove_chipcode_copy.patch"
+TARGET="${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/sync_snap_v2.sh"
+patch --batch -N $TARGET $PATCHFILE
+
+# ~/build_scripts/sync_repos.sh -u $MANI_REPO -b $MANI_BRANCH -m $MANI_XML
 
 # #configure kernel directories
 # ~/build_scripts/setup_kernel.sh
