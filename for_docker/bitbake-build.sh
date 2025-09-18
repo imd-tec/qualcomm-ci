051a2b675c7e56339abc566496a2efd6cec01af5
Jaime <jaime.spencer@imd-tec.com>
2025-09-18 12:35:01 +0100

    Corrected script paths

--- for_docker/bitbake-build.sh
+++ for_docker/bitbake-build.sh
@@ -2,7 +2,7 @@
 #Commands
 set -euo pipefail
 
-echo "\n\n====================BITBAKE-BUILD COMMANDS========================="
+echo "====================BITBAKE-BUILD COMMANDS========================="
 echo "DETAILS"
 echo "CONTAINER PWD: $(pwd)"
 echo "CONTAINER USER IDs: $(id)"
@@ -19,28 +19,28 @@ ls -la
 
 
 #CREATE NETRC FILE
-printf "\n\nCREATING NETRC"
+echo "CREATING NETRC"
 bash /home/dev/tools/build_netrc.sh
 
 #PATCH AND SYNCHRONISE REPOS
 tar -xf /home/dev/Qualcomm/patches.tar.gz -C /home/dev/Qualcomm/
 # ls /Qualcomm/
 
-printf "\n\nPATCHING"
+echo "PATCHING"
 PATCHFILE="/home/dev/Qualcomm/patches/sync_snap_v2_remove_chipcode_copy.patch" 
 TARGET="${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/sync_snap_v2.sh"
 patch --batch "$TARGET" "$PATCHFILE" 
 
-printf "\n\nSYNCHRONISING REPOS"
-bash home/dev/build_scripts/sync_repos.sh -u $MANI_REPO -b $MANI_BRANCH -m $MANI_XML
+echo "SYNCHRONISING REPOS"
+bash /home/dev/build_scripts/sync_repos.sh -u $MANI_REPO -b $MANI_BRANCH -m $MANI_XML
 
 #CONFIGURE KERNEL DIRECTORIES
-printf "\n\nCONFIGURING KERNEL DIRECTORIES"
-bash home/dev/build_scripts/setup_kernel.sh
+echo "CONFIGURING KERNEL DIRECTORIES"
+bash /home/dev/build_scripts/setup_kernel.sh
 
 #APPLY IMDT PATCHES TO QC SOURCE
-printf "\n\nAPPLY IMDT PATCHES TO QC SOURCE"
+echo "APPLY IMDT PATCHES TO QC SOURCE"
 cd ${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc
-bash home/dev/imdt-patch-qcs8550-build.sh
+bash /home/dev/imdt-patch-qcs8550-build.sh
 
 
