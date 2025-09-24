467fa98e59500a00b9dc5a69b459731390cd1925
Jaime <jaime.spencer@imd-tec.com>
2025-09-24 10:17:00 +0100

    Added sync repos step

--- for_docker/docker_entry_point.sh
+++ for_docker/docker_entry_point.sh
@@ -5,17 +5,17 @@ PREV_GID=$(id -g $USER_NAME)
 HOST_UID=$1
 HOST_GID=$2
 
+#consume UID/GID arguments
 if [[ "${1-}" == "$HOST_UID" && "${2-}" == "$HOST_GID" ]]; then
   shift 2 || true
 fi
 
-
 if [[ "$HOST_UID" != "$PREV_ID" || "$HOST_GID" != "$PREV_GID" ]]; then
-    echo "Inheriting UIDs/GIDs from the Host"
+    echo "[entrypoint] Inheriting UIDs/GIDs from the Host ($HOST_UID)"
     # Update the user ID and group ID for $USER_NAME
     groupmod -g $HOST_GID $USER_NAME
     usermod -u $HOST_UID $USER_NAME
-    echo "Changing /home/dev ownership"
+    echo "[entrypoint] changing /home/dev ownership"
     #chown home directory to new IDs
     path="/home/$USER_NAME/"
     if [[ -e "$path" ]]; then
@@ -26,17 +26,16 @@ if [[ "$HOST_UID" != "$PREV_ID" || "$HOST_GID" != "$PREV_GID" ]]; then
         # find /home/dev -print0 | xargs -0 chown -h $HOST_UID:$HOST_GID
 
     fi
-     # Create new group with same GID as original dev
+     #create new group with same GID as original dev
     groupadd -g ${PREV_GID} dev2
     usermod -a -G dev2 dev
 fi
 
-echo "[entrypoint] READY - dev IDS: (uid=$(id -u dev), gid=$(id -g dev))"
+echo "[entrypoint] READY - dev user IDS: (uid=$(id -u dev), gid=$(id -g dev))"
 
-#create new shell, preserve environment (-E), set $HOME (-H) and run as dev.
-
-# sudo -u dev bash
-
+#send signal file to continue with workflow
 mkdir -p /tmp
 echo "READY $(date -Is)" > /tmp/ready
+
+#create new shell, preserve environment (-E), set $HOME (-H) and run as dev.
 exec sudo -E -H -u dev -- sleep infinity
