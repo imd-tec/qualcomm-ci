953d34d978a1af378f9ef576b6083a49a4555b72
Jaime <jaime.spencer@imd-tec.com>
2025-10-10 14:17:04 +0100

    Bypassing need to change to private workspace by directly running commands inline rather than relying on script in worksapce

--- stop-container.sh
+++ stop-container.sh
@@ -1,50 +1,14 @@
-container_name=$1
+CONTAINER_NAME=$1
 DOCKER_VER=$2
+
+#remove container if present
+docker rm -f "$CONTAINER_NAME" 2>/dev/null || true
 
 #remove image
 docker rmi -f "imdt-qualcomm-ci:${DOCKER_VER}" 2>/dev/null || true
 
-# Check if container is running
-echo "Checking status of container '${container_name}'..."
-container_id=$(docker ps -q -f name="${container_name}")
-
-if [[ -z "$container_id" ]]; then
-    echo "Container '${container_name}' is not running."
-    exit 0
-fi
-
-# Attempt to stop the container
-echo "Stopping container '${container_name}'..."
-# Attempt to stop the container gracefully with a SIGTERM signal
-if docker stop -t 5 "${container_name}" &> /dev/null; then
-    echo "Successfully sent SIGTERM to container '${container_name}'."
-else
-    echo "Failed to stop container '${container_name}' gracefully. Proceeding with docker kill."
-fi
-
-# Check if container is running
-echo "Checking status of container '${container_name}'..."
-container_id=$(docker ps -q -f name="${container_name}")
-
-if [[ -n "$container_id" ]]; then
-    echo "killing container '${container_name}'..."
-    # Kill the container forcefully if it hasn't stopped
-    if docker kill "${container_name}" &> /dev/null; then
-        echo "Container '${container_name}' killed successfully."
-    else
-        echo "Failed to execute kill on container '${container_name}'."
-    fi
-else
-    echo "container not running"
-    exit 0
-fi
-
-echo "Checking status of container '${container_name}'..."
-container_id=$(docker ps -q -f name="${container_name}")
-if [[ -n "$container_id" ]]; then
-    echo "Failed: container '${container_name}' still running?."
-    exit 1
-else
-    echo "container not running"
-    exit 0
+#verify that container status
+if docker ps -q -f "name=^/${CONTAINER_NAME}$" | grep -q .; then
+echo "ERROR: container '$CONTAINER_NAME' still running"
+exit 1
 fi
