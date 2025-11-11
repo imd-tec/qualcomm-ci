6509b186a7fc9183a067f05a08993a1fc58fe719
jaimespencer-imdt <jaime.spencer@imd-tec.com>
2025-11-11 20:13:12 +0000

    Added manifest repo path as argument to to ensure that correct repo is being accesesed for diff-check

--- scripts/get-changed-manifests.sh
+++ scripts/get-changed-manifests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # For use exclusively when callee manifest repository build workflow is triggered by push events
-# This script takes BEFORE_SHA (previous sha) and CURRENT_SHA (latest sha) commit hashes for the given trigger event as arguments
+# This script takes BEFORE_SHA (previous sha) and CURRENT_SHA (latest sha) for the manifest repo commit hashes for the given trigger event as arguments
 # It perfomes a diff check between the commits to determine which manifest files have been added, modified, copied or renamed
+# Because this script is located in qualcomm-ci, the manifest repository path must be provided as an argument for correct diff checking
 # Usage:
-# get-manifest-diff.sh -before_sha <previous_commit_sha> -current_sha <latest_commit_sha>
+# get-manifest-diff.sh -before_sha <previous_commit_sha> -current_sha <latest_commit_sha> -manifest_repo_path <path_to_manifest_repo>
 # Outputs:
 # A newline separated list of changed manifest files written to $RUNNER_TEMP/manifests.txt
 while [[ $# -gt 0 ]]; do
@@ -12,6 +13,8 @@ while [[ $# -gt 0 ]]; do
       BEFORE_SHA="$2"; shift 2 ;;
     --current_sha)
       CURRENT_SHA="$2"; shift 2 ;;
+    --manifest_repo_path)
+      MANIFEST_REPO_PATH="$2"; shift 2 ;;
     *)
       echo "Unknown option: $1"; exit 1 ;;
   esac
@@ -47,10 +50,9 @@ fi
 echo "Checking for changed XML files between $BEFORE_SHA and $CURRENT_SHA..."
 git config --get remote.origin.url
 git log -2
-ls -l
-ls ..
+#must specify manifest repo path with -C for diff check
 mapfile -t MANIFESTS < <(git diff --name-only --diff-filter=AMCR \
-        "$BEFORE_SHA" "$CURRENT_SHA" \
+        "$BEFORE_SHA" "$CURRENT_SHA" -C "$MANIFEST_REPO_PATH" \
         | grep -E '\.xml$' \
         | grep -v '^qualcomm-ci/' || true)
 
