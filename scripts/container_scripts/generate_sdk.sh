53168472e326e2649ccb48f820e50bbc2bea712e
jaimespencer-imdt <jaime.spencer@imd-tec.com>
2025-11-23 11:28:59 +0000

    Combined setup_cache.sh and setup_yocto_env.sh into one singular script.

--- scripts/container_scripts/generate_sdk.sh
+++ scripts/container_scripts/generate_sdk.sh
@@ -8,7 +8,6 @@
 DISTRO_NAME="${DISTRO}"
 
 #set Yocto environment
-source /home/dev/tools/container_scripts/setup_cache.sh
 source /home/dev/tools/container_scripts/setup_yocto_env.sh
 
 bitbake ${IMAGE} -c populate_sdk
--- scripts/container_scripts/setup_yocto_env.sh
+++ scripts/container_scripts/setup_yocto_env.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 #=============================================================================================================================================================================
-#title: create-docker-build-env.sh
-#description: sets up yocto build environment
+#title: setup_yocto_env.sh
+#description:   sets up yocto build environment
+#               Additionally sets up shared download and sstate cache directories inside the container for development builds.
 #=============================================================================================================================================================================
 
+if [ "${BUILD_VERSION}" = "development" ]; then
+    echo "Setting up shared caches for development build..."
+    export BB_ENV_PASSTHROUGH_ADDITIONS="DL_DIR SSTATE_DIR"
+    export DL_DIR="/home/dev/downloads"
+    export SSTATE_DIR="/home/dev/sstate-cache"
+    echo "Using shared caches (DL_DIR=$DL_DIR, SSTATE_DIR=$SSTATE_DIR)"
+
+fi
+echo "Setting up Yocto build environment..."
 cd "${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc" || exit 1
 pyenv global "$PYENV"
 export MACHINE=$MACHINE
