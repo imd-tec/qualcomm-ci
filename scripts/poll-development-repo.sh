754c2606593669ec2f3606d69618a4b71e62feef
jaimespencer-imdt <jaime.spencer@imd-tec.com>
2025-12-01 18:42:22 +0000

    Refactored to allow for logging of multiple modified repositories. Ensured that branches with the same name across different repos are handled correctly.

--- scripts/poll-development-repo.sh
+++ scripts/poll-development-repo.sh
@@ -7,14 +7,24 @@
 #     Script is accessed from the qualcomm-ci repository so takes <path_to_manifest_repo> as an argument to access development manifests. 
 #     As a development manifest can refer to a development meta layer branch, as opposed to a revision commit hash, 
 #     this script must clone the given  meta layer repository and determine if relevant build files have been changed since the previous cron job.
-#     The last checked commit hash is stored in a local file for each project branch being monitored (see /mnt/nvme1/qcom_ci/dev_repo_poll/state/)
+#     The last checked commit hash is stored in a local state file for each project branch being monitored (see /mnt/nvme1/qcom_ci/dev_repo_poll/state/)
 #     If changes are detected, the script outputs the path to the development manifest to $GITHUB_OUTPUT and continues the build process.
+#
+#     Additionally, A monitored repository labelled 'FAILED' will attempt to build irrespective of if there have been
+#     any changes since the last attempt. Inversely, builds labelled as 'SUCCESS' will not build. 
 #usage:
 #     poll_development_repo.sh --manifest_repo_path <path_to_manifest_repo>
 #outputs:
 #     A txt file containing the path to the development manifest
 #=============================================================================================================================================================================
 set -eu
+
+DEV_REPO_CACHE_PATH="/mnt/nvme1/qcom_ci/dev_repo_poll/cache"
+DEV_REPO_STATE_PATH="/mnt/nvme1/qcom_ci/dev_repo_poll/state"
+RELEVANT_FILES='^(conf/|recipes-|tools/|patches/|contents\.xml$)'
+manifest_path=""
+development_branches=""
+trigger_build=false
 
 function parse_args() {
   while [[ $# -gt 0 ]]; do
@@ -23,16 +33,9 @@ function parse_args() {
         MANIFEST_REPO_PATH="$2"; shift 2 ;;
       *)
         echo "Unknown option: $1"; exit 1 ;;
-    esac
+    esac 
   done
 }
-
-
-DEV_REPO_CACHE_PATH="/mnt/nvme1/qcom_ci/dev_repo_poll/cache"
-DEV_REPO_STATE_PATH="/mnt/nvme1/qcom_ci/dev_repo_poll/state"
-RELEVANT_FILES='^(conf/|recipes-|tools/|patches/|contents\.xml$)'
-manifest_path=""
-development_branches=""
 
 function get_manifest_path() {
     #find the development manifest file in the repository
@@ -43,8 +46,6 @@ function get_manifest_path() {
         exit 1
     fi
     echo "Found development manifest: $manifest_path"
-
-    echo "$manifest_path"
 }
 
 function get_development_revisions() {
@@ -66,80 +67,118 @@ function get_development_revisions() {
 
 function check_for_differences() {
     for branch in $development_branches; do
-        echo -e "Checking for changes in branch: $branch\n"
 
-        #get meta layer project name for this branch
-        repo_name=$(xmllint --xpath "string(//project[@revision='$branch']/@name)" "$manifest_path" ) #meta-imdt-qcom-dev
-        #TODO - Handling multiple projects with the same branch name (unlikely but possible)
-        #get remote from first project with this branch
-        remote=$(xmllint --xpath "string(//project[@name='$repo_name']/@remote)" "$manifest_path") #imdt
-        #get fetch org url from remote
-        base_url=$(xmllint --xpath "string(//remote[@name='$remote']/@fetch)" "$manifest_path") #https://github.com/imd-tec"
+        #must handle the fact that some meta layer repositories may have the same branch name
+        #name="meta-imdt-qcom-dev" name="clo/le/meta-qti-gst" ... => meta-imdt-qcom-dev clo/le/meta-qti-gst ... 
+        repo_names=$(xmllint --xpath "//project[@revision='$branch']/@name" "$manifest_path" \
+                    | grep -o 'name=\"[^\"]*\"' \
+                    | cut -d'"' -f2)
 
-        #construct repo url
-        repo_url="${base_url}/${repo_name}.git"
+        for repo_name in $repo_names; do
+            echo -e "Checking for changes in $repo_name($branch)\n"
+            #get remote from first project with this branch
+            remote=$(xmllint --xpath "string(//project[@name='$repo_name']/@remote)" "$manifest_path") #imdt
+            #get fetch org url from remote
+            base_url=$(xmllint --xpath "string(//remote[@name='$remote']/@fetch)" "$manifest_path") #https://github.com/imd-tec"
 
-        #ensure cache and state directories exist
-        mkdir -p "${DEV_REPO_CACHE_PATH}" "${DEV_REPO_STATE_PATH}" 
-        
-        #clone repo into local cache if not already cached
-        if [ ! -d "$DEV_REPO_CACHE_PATH/$repo_name" ]; then
-            git clone "$repo_url" "$DEV_REPO_CACHE_PATH/$repo_name"
-        fi
+            #construct repo url
+            repo_url="${base_url}/${repo_name}.git"
 
-        #get current hash for the branch
-        cd "$DEV_REPO_CACHE_PATH/$repo_name"
-        git fetch origin "$branch"
-        current_hash=$(git rev-parse origin/"$branch")
-
-        #attempt to load previous hash from state file
-        state_file="$DEV_REPO_STATE_PATH/$repo_name.last"
-        previous_hash=""
-
-        #if state file exists, read previous hash
-        if [ -f "$state_file" ]; then
-            #read line from state file (i.e., "Wed 26 Nov 18:32:24 GMT 2025 8aa69f5....")
-            line=$(cat "$state_file") 
-            #extract hash from line 
-            previous_hash=${line##* } 
-            #extract date from state file for logging purposes
-            last_date="${line% "$previous_hash"}" 
-            echo -e "\nLast checked on $last_date."
-        fi
-
-        if [ "$previous_hash" == "$current_hash" ]; then
-            echo -e "\nNo changes detected in branch: $branch\nPrevious hash  $previous_hash\nCurrent hash   $current_hash\n\nUp to date. Skipping build trigger."    
-        fi
-        
-        #if previous_hash exists, perform diff check to determine if relevant files have changed
-        if [ -n "$previous_hash" ] && [ "$previous_hash" != "$current_hash" ]; then
-            echo "Comparing files between $previous_hash and $current_hash:"
-            files=$(git diff --name-only "$previous_hash" "$current_hash")
+            #ensure cache and state directories exist
+            mkdir -p "${DEV_REPO_CACHE_PATH}" "${DEV_REPO_STATE_PATH}" 
             
-            #only continue with build if relevant files, as previously outlined, have been changed
-            include=$(printf "%s\n" "$files" | grep -E "$RELEVANT_FILES" || true)
-            if [ -z "$include" ]; then
-                echo "No relevant files were modified. Skipping build trigger."
-            else
-                echo "Relevant changes detected:"
-                echo "$include"
-                echo "Continuing build process..."
-                echo "$manifest_path" >> "$RUNNER_TEMP/manifests.txt"
+            #clone repo into local cache if not already cached
+            if [ ! -d "$DEV_REPO_CACHE_PATH/$repo_name" ]; then
+                git clone "$repo_url" "$DEV_REPO_CACHE_PATH/$repo_name"
             fi
-        fi
 
-        #if previous_hash does not exist; it's the first build => continue with build
-        if [ -z "$previous_hash" ]; then
-            echo -e "\nNo previous state found for $repo_name. Assuming first build. Continuing build process..." 
-            echo "$manifest_path" >> "$RUNNER_TEMP/manifests.txt"
-        fi
+            #get current hash for the branch
+            cd "$DEV_REPO_CACHE_PATH/$repo_name"
+            git fetch origin "$branch"
+            current_hash=$(git rev-parse origin/"$branch")
 
-        #echo current_hash hash to state file for scheduled comparison
-        echo "$(date) $current_hash" > "$state_file"
+            #attempt to load previous hash from state file
+            repo_file_name=${repo_name//\//_} # clo/le/meta-qti-gst => clo_le_meta-qti-gst
+            state_file="$DEV_REPO_STATE_PATH/$repo_file_name.$branch.last"
+
+            last_date=''
+            last_hash=''
+            last_result=''
+            
+            #if state file exists, read previous hash and success status
+            if [ -f "$state_file" ]; then
+                #date, hash and status from state .last file (i.e., "Wed 26 Nov 18:32:24 GMT 2025 | 8aa69f5.... | SUCCESS")
+                IFS='|' read -r last_date last_hash last_result < "$state_file"
+                echo -e "\nLast attempted build on $last_date."
+            fi
+
+            #if last_hash does not exist; it's the first build => continue with build
+            if [ -z "$last_hash" ]; then
+                echo -e "\nNo previous state found for $repo_name. Assuming first build. Continuing build process..." 
+                update_output "$state_file" "$current_hash"
+                continue
+            fi
+
+            #if there is no detected change (i.e., the current and last hashes are the same) and the previous result was a SUCCESS => continue to next candidate
+            if [ "$last_hash" == "$current_hash" ]  && [ "$last_result" == "SUCCESS" ]; then
+                echo -e "\nPrevious build was labelled $last_result and no changes detected in:\n $repo_url ($branch)\nPrevious hash  $last_hash\nCurrent hash    $current_hash\n\nUp to date. Skipping build trigger."    
+                continue
+            fi
+
+            #if there is no detected change, BUT the previous run was a failure => continue with build
+            if [ "$last_hash" == "$current_hash" ]  && [ "$last_result" == "FAILURE" ]; then
+                echo -e "\nNo new changes since last build attempt, but previous build was labelled $last_result. Attempting to build again..."
+                update_output "$state_file" "$current_hash"
+                continue
+            fi       
+            
+            #if there is a detected change, perform diff check to determine if meaningful build files have been affected
+            if [ -n "$last_hash" ] && [ "$last_hash" != "$current_hash" ]; then
+                echo "Comparing files between $last_hash and $current_hash:"
+                files=$(git diff --name-only "$last_hash" "$current_hash")
+                
+                #only continue with build if relevant files have been changed
+                include=$(printf "%s\n" "$files" | grep -E "$RELEVANT_FILES" || true)
+                if [ -z "$include" ]; then
+                    echo "No relevant files were modified. Skipping build trigger."
+                    continue
+                else
+                    echo -e "\nRelevant changes detected:\n$include\nContinuing build process..."
+                    update_output "$state_file" "$current_hash"
+                    continue
+                fi
+            fi
+        done
+
     done
+    
+    #if the build has been set to trigger, append manifest path to manifests.txt to signal build in later steps 
+    if $trigger_build; then
+        echo "$manifest_path" >> "$RUNNER_TEMP/manifests.txt"
+    fi
+
+    #send accumulated repository details to output for later logging job
+    if [ -f "$RUNNER_TEMP/repo_states.txt" ]; then
+    {
+        echo "repo_states<<EOF"
+        cat "$RUNNER_TEMP/repo_states.txt"
+        echo "EOF"
+    } >> "$GITHUB_OUTPUT"
+    fi
+}
+
+
+function update_output() {
+        local state_file="$1"
+        local current_hash="$2"
+ 
+        trigger_build=true
+        #append details of triggering repository and state file for later logging purposes
+        echo "${state_file}|${current_hash}" >> "$RUNNER_TEMP/repo_states.txt"
 }
 
 parse_args "$@"
 get_manifest_path
 get_development_revisions
 check_for_differences
+
