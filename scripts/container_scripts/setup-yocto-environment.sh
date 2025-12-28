0b677e4d2e163ecadfd326168f16dfc1b549c007
jaimespencer-imdt <jaime.spencer@imd-tec.com>
2025-12-28 17:16:29 +0000

    Append BB cache paths to auto.conf.

--- scripts/container_scripts/setup-yocto-environment.sh
+++ scripts/container_scripts/setup-yocto-environment.sh
@@ -5,7 +5,6 @@
 #               Additionally sets up shared download and sstate cache directories inside the container for development builds.
 #=============================================================================================================================================================================
 
-
 echo "Setting up Yocto build environment..."
 cd "${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc" || { echo "ERROR: Failed to change directory to ${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc"; exit 1; }
 pyenv global "$PYENV"
@@ -15,10 +14,11 @@ source poky/qti-conf/set_bb_env.sh
 
 # if build version is development, override config cache directories 
 if [ "${BUILD_VERSION}" = "development" ]; then
-    echo "Setting up shared caches for development build..."
-    export BB_ENV_PASSTHROUGH_ADDITIONS="$BB_ENV_PASSTHROUGH_ADDITIONS DL_DIR SSTATE_DIR"
-    export DL_DIR="/home/dev/downloads"
-    export SSTATE_DIR="/home/dev/sstate-cache"
+    echo "Appending CI cache paths to conf/auto.conf..."    
+cat >> conf/auto.conf <<EOF
+DL_DIR = "/home/dev/downloads"
+SSTATE_DIR = "/home/dev/sstate-cache"
+EOF
 fi
 
 echo "Yocto environment setup completed"
