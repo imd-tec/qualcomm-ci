2ec3cd1821893b7dc614a96c6759c59162da2364
jaimespencer-imdt <jaime.spencer@imd-tec.com>
2026-01-05 16:26:06 +0000

    Rename SDK filename var for accuracy. Moved release directory outside of SDK_FILENAME for accuracy. Correct comments.

--- scripts/container_scripts/generate-sdk.sh
+++ scripts/container_scripts/generate-sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #=============================================================================================================================================================================
 #title: generate-sdk.sh
-#description: Generate a Yocto SDK for the specified image, rename and move to the release directory.
+#description: Generate a Yocto SDK for the specified image, and locate, rename and move it to the release directory.
 #=============================================================================================================================================================================
 
 #cache distro name as yocto unsets the variable
@@ -15,15 +15,7 @@ bitbake "${IMAGE}" -c populate_sdk
 #locate generated SDK script and rename
 SDK_DIR="${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/build-${DISTRO_NAME}/tmp-glibc/deploy/sdk/"
 
-RELEASE_DIR="/home/dev/Qualcomm/release"
-if [[ ! -d "$RELEASE_DIR" ]]; then
-    mkdir -p "$RELEASE_DIR"
-fi
-
-#output name format: [release_name_without_prebuilt_suffix]-sdk-v[build_version].sh
-OUTPUT_NAME="${RELEASE_DIR}/${RELEASE_NAME%_prebuilt*}-sdk-v${BUILD_VERSION}.sh"
-
-#verify that there is exactly one sh script in SDK_DIR
+#findd the SDK script
 cd "$SDK_DIR" || exit 1
 sdk_sh="$(find -maxdepth 1 -type f -name '*.sh')"
 
@@ -32,5 +24,14 @@ if [[ -z "$sdk_sh" ]]; then
     exit 1
 fi
 
-#rename SDK script to output_name
-mv "$sdk_sh" "$OUTPUT_NAME"
+#ensure release directory exists
+RELEASE_DIR="/home/dev/Qualcomm/release"
+if [[ ! -d "$RELEASE_DIR" ]]; then
+    mkdir -p "$RELEASE_DIR"
+fi
+
+#output name format: [release_name_without_prebuilt_suffix]-sdk-v[build_version].sh
+SDK_FILENAME="${RELEASE_NAME%_prebuilt*}-sdk-v${BUILD_VERSION}.sh"
+
+#rename and move the SDK script to the release directory
+mv "$sdk_sh" "$RELEASE_DIR/$SDK_FILENAME"
