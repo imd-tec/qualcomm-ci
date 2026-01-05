632e6ae37e0799e35ca04e2b7547417d4d75ec0c
jaimespencer-imdt <jaime.spencer@imd-tec.com>
2026-01-05 16:15:24 +0000

    Correct by moving SDK to release directory

--- scripts/container_scripts/generate-sdk.sh
+++ scripts/container_scripts/generate-sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #=============================================================================================================================================================================
 #title: generate-sdk.sh
-#description: Generate a Yocto SDK for the specified image and rename the resulting SDK installer script.
+#description: Generate a Yocto SDK for the specified image, rename and move to the release directory.
 #=============================================================================================================================================================================
 
 #cache distro name as yocto unsets the variable
@@ -15,8 +15,13 @@ bitbake "${IMAGE}" -c populate_sdk
 #locate generated SDK script and rename
 SDK_DIR="${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/build-${DISTRO_NAME}/tmp-glibc/deploy/sdk/"
 
+RELEASE_DIR="/home/dev/Qualcomm/release"
+if [[ ! -d "$RELEASE_DIR" ]]; then
+    mkdir -p "$RELEASE_DIR"
+fi
+
 #output name format: [release_name_without_prebuilt_suffix]-sdk-v[build_version].sh
-OUTPUT_NAME="${SDK_DIR}/${RELEASE_NAME%_prebuilt*}-sdk-v${BUILD_VERSION}.sh"
+OUTPUT_NAME="${RELEASE_DIR}/${RELEASE_NAME%_prebuilt*}-sdk-v${BUILD_VERSION}.sh"
 
 #verify that there is exactly one sh script in SDK_DIR
 cd "$SDK_DIR" || exit 1
