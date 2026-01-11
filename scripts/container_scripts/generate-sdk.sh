babe65d7c51e00da1422c071d642bd7f7717e00a
jaimespencer-imdt <jaime.spencer@imd-tec.com>
2026-01-11 13:50:04 +0000

    Correct development "release" directory

--- scripts/container_scripts/generate-sdk.sh
+++ scripts/container_scripts/generate-sdk.sh
@@ -10,28 +10,33 @@ DISTRO_NAME="${DISTRO}"
 #set Yocto environment
 source /home/dev/tools/container_scripts/setup-yocto-environment.sh
 
+#generate SDK 
 bitbake "${IMAGE}" -c populate_sdk
 
-#locate generated SDK script and rename
+#locate generated SDK script
 SDK_DIR="${QCOM_ROOT_DIR}/LE.PRODUCT.2.1.r1/apps_proc/build-${DISTRO_NAME}/tmp-glibc/deploy/sdk/"
-
-#findd the SDK script
 cd "$SDK_DIR" || exit 1
 sdk_sh="$(find -maxdepth 1 -type f -name '*.sh')"
-
 if [[ -z "$sdk_sh" ]]; then
     echo "ERROR: No SDK files found in ${SDK_DIR}"
     exit 1
 fi
 
+#define release directory based on build type
+if [ "$BUILD_VERSION" == "development" ]; then
+    RELEASE_DIR="/home/dev/Qualcomm/release/${REPO_NAME}"
+else
+    RELEASE_DIR="/home/dev/Qualcomm/release"
+fi
+
 #ensure release directory exists
-RELEASE_DIR="/home/dev/Qualcomm/release"
 if [[ ! -d "$RELEASE_DIR" ]]; then
     mkdir -p "$RELEASE_DIR"
 fi
 
-#output name format: [release_name_without_prebuilt_suffix]-sdk-v[build_version].sh
-SDK_FILENAME="${RELEASE_NAME%_prebuilt*}-sdk-v${BUILD_VERSION}.sh"
+
 
 #rename and move the SDK script to the release directory
+#output name format: [release_name_without_prebuilt_suffix]-sdk-v[build_version].sh
+SDK_FILENAME="${RELEASE_NAME%_prebuilt*}-sdk-v${BUILD_VERSION}.sh"
 mv "$sdk_sh" "$RELEASE_DIR/$SDK_FILENAME"
